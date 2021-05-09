a89ce67769254d0b7a5bdecd064c43614573eb94
edwinisnow <edwin04@gmail.com>
2021-05-09 20:14:18 +0100

    Formik Control, Container, Input, Textarea

---
+++ react-formik-forms/src/components/FormikContainer.js
@@ -0,0 +1,39 @@
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import FormikControl from "./FormikControl";
+
+function FormikContainer() {
+  const initialValues = { email: "", description: "" };
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Required"),
+    description: Yup.string().required("Required"),
+  });
+  const onSubmit = (values) => console.log("Form data", values);
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {(formik) => (
+        <Form>
+          <FormikControl
+            control="input"
+            type="email"
+            label="Email"
+            name="email"
+          />
+          <FormikControl
+            control="textarea"
+            name="description"
+            label="Description"
+          />
+
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default FormikContainer;
