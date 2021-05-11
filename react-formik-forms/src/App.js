f5f6cadb2976404a13232e871de51430c7f209f8
edwinisnow <edwin04@gmail.com>
2021-05-11 15:01:20 +0100

    Login, Registration Form

--- react-formik-forms/src/App.js
+++ react-formik-forms/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import "./App.css";
 import FormikContainer from "./components/FormikContainer";
+import LoginForm from "./components/LoginForm";
+import RegistrationForm from "./components/RegistrationForm";
 
 function App() {
   return (
     <div className="App">
-      <FormikContainer />
+      <RegistrationForm />
     </div>
   );
 }
