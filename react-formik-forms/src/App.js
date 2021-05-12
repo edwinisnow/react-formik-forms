a9d7c483f168ff5ea3ef98d57fca3c633f92b207
edwinisnow <edwin04@gmail.com>
2021-05-12 16:14:52 +0100

    Enrollment Form

--- react-formik-forms/src/App.js
+++ react-formik-forms/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import "./App.css";
+import EnrollmentForm from "./components/EnrollmentForm";
 import FormikContainer from "./components/FormikContainer";
 import LoginForm from "./components/LoginForm";
 import RegistrationForm from "./components/RegistrationForm";
@@ -7,7 +8,7 @@ import RegistrationForm from "./components/RegistrationForm";
 function App() {
   return (
     <div className="App">
-      <RegistrationForm />
+      <EnrollmentForm />
     </div>
   );
 }
